ff695d5ee42f5bdaf47899ff5991fac5f5350891
Nicolas Juen <njuen@beapi.fr>
2020-05-20 18:31:41 +0200

    Unify modifications from scaffold-plugins

--- src/Command/ScaffoldThemeCommand.php
+++ src/Command/ScaffoldThemeCommand.php
@@ -217,7 +217,7 @@ class ScaffoldThemeCommand extends BaseCommand {
 
 			try {
 				$composerJson = $composerFile->read();
-				$composerJson['autoload']['psr-4'][$themeNamespace."\\"] = str_replace( '//', '/', $themePath.'/inc/' );
+				$composerJson['autoload']['psr-4'][$themeNamespace."\\"] = rtrim( $themePath, '/' ) .'/inc/';
 
 
 				$composerFile->write( $composerJson );
